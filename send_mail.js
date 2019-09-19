c03af4060e3ee03ec67c030369356fe5d35e8f25
Aaditya Raj <37104890+Aadityapritam@users.noreply.github.com>
2019-09-20 02:18:27 +0530

    Add files via upload

---
+++ send_mail.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+var nodemailer = require('nodemailer');
+var server = express();
+
+ser
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'aadityapritam00@gmail.com',
+        pass: '**********'
+    }
+});
+
+var mailoptions = {
+    from: 'aadityapritam00@gmail.com',
+    to: 'aadityaraj.sistec@gmail.com',
+    subject: 'mail using Node.js',
+    text: 'hi, this mail is checking automatically by node.js'
+};
+
+transporter.sendMail(mailoptions, function (error, info) {
+    if (error) { console.log(error,'Email not sent, something went wrong! '); }
+    else {
+        console.log('Email sent by Node.js:' + info.response);
+    }
+});
